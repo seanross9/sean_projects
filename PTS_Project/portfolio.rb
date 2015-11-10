d01fb9d03fbfcf54fd30295209b785c7ff246034
seanross9 <ross.s@husky.neu.edu>
2015-11-09 21:34:24 -0500

    API integration
    simplified portfolio class to take one initial list. built methods
    within portfolio class to read current stock price from an excel sheet
    or API.

--- PTS_Project/portfolio.rb
+++ PTS_Project/portfolio.rb
@@ -1,4 +1,6 @@
 require 'roo'
+require 'httparty'
+require 'json'
 
 class StockHolding
 	def initialize(index, shares, price)
@@ -9,8 +11,8 @@ class StockHolding
 end
 
 class Portfolio
-	def initialize(initial_list, current_list)
-		@initial_list, @current_list = initial_list, current_list
+	def initialize(initial_list)
+		@initial_list = initial_list
 	end
 
 	def get_stock_value(list)
@@ -21,27 +23,61 @@ class Portfolio
 		value
 	end
 
+	def load_prices_from_excel(filename)
+		@price_lookup = {}
+		new_file = ExcelFile.new(filename)
+		current_list = new_file.read_sheet('Oct 26 2015')
+		current_list.each do |stock|
+			@price_lookup[stock.index] = stock.price
+		end
+	end
+
+	def get_current_portfolio_value
+		value = 0
+		@initial_list.each do |stock|
+			value += stock.shares * @price_lookup[stock.index]
+		end
+		value
+	end
+
 	def performance_report
 		initial_investment = get_stock_value(@initial_list)
-		portfolio_value = get_stock_value(@current_list)
+		portfolio_value = get_current_portfolio_value
 		printf("Total Initial Investment:   $ %9.2f\n", initial_investment)
 		printf("Current Portfolio Value:    $ %9.2f\n", portfolio_value)
 		printf("Total ROI:                  $ %9.2f\n", total_return = portfolio_value - initial_investment)
 		printf("Average Annual ROI:         $ %9.2f\n", total_return/@initial_list.length)
 	end
-end
 
-def open_file(filename)
-	@file = Roo::Spreadsheet.open(filename)
-end
-
-def read_sheet(sheetname)
-	stock_list = []
-	@file.sheet(sheetname).each do |row|
-		row.inspect
-		 stock_list << StockHolding.new(*row)
+	def get_price_for_symbol(stock_ticker)
+		#puts stock_ticker.inspect
+		url = "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol="+stock_ticker
+		stock = HTTParty.get(url)
+		my_hash = JSON.parse(stock.body)
+		my_hash["LastPrice"]
 	end
-	stock_list
+
+	def load_prices_from_api
+		@price_lookup = {}
+		@initial_list.each do |stock|
+			@price_lookup[stock.index] = get_price_for_symbol(stock.index)
+		end
+	end
+end
+
+class ExcelFile
+	def initialize(filename)
+		@file = Roo::Spreadsheet.open(filename)
+	end
+
+	def read_sheet(sheetname)
+		stock_list = []
+		@file.sheet(sheetname).each do |row|
+			row.inspect
+		 	stock_list << StockHolding.new(*row)
+		end
+		stock_list
+	end
 end
 
 puts 'Welcome to Your Portfolio Tracking System.'
@@ -51,13 +87,14 @@ puts '[a] View Asset Allocation Report'
 choice = gets.chomp
 
 if choice == 'p'	
-	open_file('./stock_prices.xlsx')
-	current_stock_list = read_sheet('Oct 26 2015')
-	initial_stock_list = read_sheet('Jan 4 2010')
-
-	my_portfolio = Portfolio.new(initial_stock_list, current_stock_list)
+	new_file = ExcelFile.new('./stock_prices.xlsx')
+	initial_stock_list = new_file.read_sheet('Jan 4 2010')
+	my_portfolio = Portfolio.new(initial_stock_list)
+	#my_portfolio.load_prices_from_excel('./stock_prices.xlsx')
+	my_portfolio.load_prices_from_api
 	puts my_portfolio.performance_report
 end
+
 
 
 
